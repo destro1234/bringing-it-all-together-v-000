c93ea5e026a4b7acf552b4773f28a5dc613f102b
Elias Ventura <eliasventura23@gmail.com>
2019-01-19 18:03:00 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -34,8 +34,9 @@ class Dog
     VALUES (?, ?)
     SQL
 
-    DB[:conn].execute(sql, self.name, self.breed)
+    row= DB[:conn].execute(sql, self.name, self.breed)
     @id = DB[:conn].execute("SELECT last_insert_rowid()")[0][0]
+    self.new(@id, row[0], row[1])
   end
 
 end
