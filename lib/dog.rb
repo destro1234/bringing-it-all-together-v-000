85b02861754ddee21e6e7c6383b0fe83a424b2ee
Elias Ventura <eliasventura23@gmail.com>
2019-01-19 18:34:56 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -69,4 +69,14 @@ class Dog
       Dog.create(name: name, breed: breed)
     end
   end
+
+  def update
+    sql = <<-SQL
+    UPDATE dogs
+    SET name= ?, breed= ?
+    WHERE id = ?
+    SQL
+
+    DB[:conn].execute(sql,self.name, self.breed, self.id)
+  end
 end
